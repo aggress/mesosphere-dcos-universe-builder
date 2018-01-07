1feba7e333bc1f8498325f590f393bf8be04264b
Richard Shaw <richard@aggress.net>
2018-01-07 12:30:27 +0000

    Updated Readme

--- scripts/prod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t universe-builder .
-docker rm -f /builder
-docker rm -f /registry
-docker run --name=builder -p 127.0.0.1:5001:5001 -v /var/run/docker.sock:/var/run/docker.sock universe-builder:latest
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t universe-builder .
 docker ps --format '{{.Names}}' | grep registry || docker rm -f /registry > /dev/null 2>&1
 docker ps --format '{{.Names}}' | grep builder || docker rm -f /builder > /dev/null 2>&1
-docker run --name=builder -p 127.0.0.1:5001:5001 -v /var/run/docker.sock:/var/run/docker.sock universe-builder:latest
+docker run --name=builder -p 127.0.0.1:5001:5001 -v /var/run/docker.sock:/var/run/docker.sock aggress/universe-builder:latest
